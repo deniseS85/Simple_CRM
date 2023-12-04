a63c7bf9e60decfd013a0fbae384d40fb6b9d439
deniseS85 <denisabel78@hotmail.de>
2023-12-04 16:24:31 +0100

    save new user in firebase database

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { User } from '../models/user.class';
-import { Firestore, addDoc } from '@angular/fire/firestore';
-import { collection } from 'firebase/firestore';
+import { Firestore } from '@angular/fire/firestore';
+import { collection, addDoc } from "firebase/firestore"; 
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -13,21 +14,39 @@ export class DialogAddUserComponent {
   user = new User();
   birthDate!: Date;
   loading = false;
+  firestore: Firestore = inject(Firestore);
 
-  constructor(private firestore: Firestore) {}
+  constructor(public dialogRef: MatDialogRef<DialogAddUserComponent>) {}
 
-  saveUser() {
-    console.log('user', this. user);
-    this.user.birthDate = this.birthDate.getTime();
-    this.loading = true;
+   async saveUser() {
+      console.log('user', this. user);
+      this.user.birthDate = this.birthDate.getTime();
+      this.loading = true;
 
-    this.firestore
-   /*  .collection('users')
+      await addDoc(this.getUserRef(), this.user.toJson()).catch(
+        (err) => {
+          console.error(err)
+        }
+      ).then(() => { 
+        this.loading = false; 
+        this.dialogRef.close();
+      })
+
+    
+  
+
+
+   /*  this.firestore */
+    /* .collection('user')
     .add(this.user.toJson())
     .then((result: any) {
       console.log(result)
     }); */
 
+  }
+
+  getUserRef(){
+    return collection(this.firestore, 'users');
   }
 
 }
