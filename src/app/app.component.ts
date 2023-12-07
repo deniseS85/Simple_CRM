a6aa7f6ca5dfd7de90c9fcf30c02385513859e2a
deniseS85 <denisabel78@hotmail.de>
2023-12-07 16:57:56 +0100

    edit filter input usertable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserTableService } from './user-table.service';
-import { user } from '@angular/fire/auth';
 
 
 @Component({
@@ -15,37 +14,39 @@ export class AppComponent {
   isUser = false;
   @ViewChild('inputValue') inputValue!: ElementRef;
 
+  
   constructor(private router: Router, public userTableService: UserTableService) {}
+
 
   activeLink(routerLink: string) {
       if (this.router.isActive(routerLink, true)) {
           this.currentLink = routerLink.charAt(0).toUpperCase() + routerLink.slice(1);
           this.isUser = this.currentLink == 'User' ? true : false;
       }
-      
       return this.router.isActive(routerLink, true);
   }
+
 
   filterUser() {
       let inputValue = this.inputValue.nativeElement.value.toLowerCase();
       let userTable = this.userTableService.userList;
-
-
+      let filteredUserList:any[] = [];
+    
       if (inputValue) {
           for (let i = 0; i < userTable.length; i++) {
-              let firstname = userTable[i].firstName.toLowerCase().substring(0, inputValue.length) == inputValue;
-              let lastname = userTable[i].lastName.toLowerCase().substring(0, inputValue.length) == inputValue;
-              let city = userTable[i].city.toLowerCase().substring(0, inputValue.length) == inputValue;
-
-              if (firstname || lastname || city) {
-                console.log(userTable[i]);
-              } 
+            let firstname = userTable[i].firstName.toLowerCase().substring(0, inputValue.length) == inputValue;
+            let lastname = userTable[i].lastName.toLowerCase().substring(0, inputValue.length) == inputValue;
+            let city = userTable[i].city.toLowerCase().substring(0, inputValue.length) == inputValue;
+      
+            if (firstname || lastname || city) {
+                filteredUserList.push(userTable[i]);
+            }
           }
       } else {
-          this.userTableService.userList = userTable;
-          console.log(userTable);
+          filteredUserList = userTable;
       }
-    }
+      this.userTableService.userList = filteredUserList;
+  }
 }
 
 
