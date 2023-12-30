03884ba58cc378675bc49abdbdb0ffc623630a88
deniseS85 <denisabel78@hotmail.de>
2023-12-30 15:23:10 +0100

    edit chart

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, inject } from '@angular/core';
 import { User } from '../models/user.class';
 import { Firestore, collection, onSnapshot } from '@angular/fire/firestore';
 import { Events } from '../models/events.class';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 
 @Component({
@@ -9,22 +11,25 @@ import { Events } from '../models/events.class';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent  {
+export class DashboardComponent implements AfterViewInit {
   firestore: Firestore = inject(Firestore);
   userList:any = [];
   unsubUserList;
   unsubEventList;
   eventsList:any[] = [];
-  monthlyEvents:any[] = [];
-  statistic!: number;
   eventsByMonth: Record<string, number> = {};
+  @ViewChild('barCanvas') private barCanvas!: ElementRef;
+  private barChart: any;
+  eventYear:any;
 
   constructor() {
       this.unsubUserList = this.subUsersList();
       this.unsubEventList = this.subEventsList();
   }
 
-  
+  ngAfterViewInit() {
+      this.createBarChart();
+  }
 
   subUsersList() {
       return onSnapshot(this.getUserRef(), (list) =>{
@@ -42,6 +47,7 @@ export class DashboardComponent  {
               this.eventsList.push(new Events().setEventsObject(element.data(), element.id));
           });
           this.updateEventsByMonth();
+          this.updateBarChart(); 
       })
   }
 
@@ -59,23 +65,104 @@ export class DashboardComponent  {
   }
 
   updateEventsByMonth() {
-      this.eventsByMonth = {};
-     
+      let allMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+      let currentDate = new Date();
+      let currentMonth = currentDate.getMonth();
+      let currentYear = currentDate.getFullYear();
+      let relevantMonths = [];
+
+      for (let i = 11; i >= 0; i--) {
+          let monthIndex = (currentMonth - i + 12) % 12;
+          relevantMonths.push(allMonths[monthIndex]);
+      }
+    
+      this.eventsByMonth = Object.fromEntries(relevantMonths.map(month => [`${month}-${currentYear}`, 0]));
+
       this.eventsList.forEach(event => {
-        const eventDate = new Date(event.day);
-        const monthYearKey = `${eventDate.getMonth() + 1}-${eventDate.getFullYear()}`;
-
-        if (this.eventsByMonth[monthYearKey]) {
-          this.eventsByMonth[monthYearKey]++;
-        } else {
-          this.eventsByMonth[monthYearKey] = 1;
-        }
+          let eventDate = new Date(event.day);
+          let eventMonth = eventDate.getMonth();
+          this.eventYear = eventDate.getFullYear();
+      
+          if (this.eventYear === currentYear && eventMonth <= currentMonth) {
+              let month = allMonths[eventMonth];
+              this.eventsByMonth[`${month}-${this.eventYear}`]++;
+          }
       });
-
-   
-    console.log('Statistik der Events nach Monat:', this.eventsByMonth);
+      console.log('Statistik der Events nach Monat:', this.eventsByMonth);
   }
 
+  createBarChart() {
+      let ctx = this.barCanvas.nativeElement.getContext('2d');
+      let allMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+      let currentDate = new Date();
+      let currentMonthIndex = currentDate.getMonth();
+      let dynamicLabels = allMonths.slice(currentMonthIndex + 1).concat(allMonths.slice(0, currentMonthIndex + 1));
+      
+      this.barChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: dynamicLabels,
+          datasets: [{
+            label: 'Patients Overview',
+            data:  Object.values(this.eventsByMonth),
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+            barThickness: 20,
+          }],
+        },
+        options: {
+          responsive: true,
+          scales: {
+              y: {
+                  beginAtZero: true,
+                  ticks: {
+                    color: 'lightgrey',
+                    stepSize: 1
+                  },
+                  min: 0,
+                  max: 10,
+                  grid: {
+                    color: 'rgb(1,103,175)' // Farbe der Gitterlinien der Y-Achse
+                  },
+              },
+              x: {
+                  ticks: {
+                    color: 'lightgrey',
+                    maxRotation: 0,
+                    minRotation: 0
+                  }
+              }
+          },
+          plugins: {
+            legend: {
+              display: false,
+            },
+            tooltip: {
+              callbacks: {
+                label: function (context) {
+                  return `Events: ${context.parsed.y}`;
+                },
+              },
+            },
+          }, 
+        }
+      });
+  }
+
+  updateBarChart() {
+    if (this.barChart) {
+      this.barChart.data.labels = Object.keys(this.eventsByMonth).map(label => {
+        const [month, year] = label.split('-');
+        return `${month} ${year.slice(2)}`;
+      });
+  
+      this.barChart.data.datasets[0].data = Object.values(this.eventsByMonth);
+      this.barChart.update();
+    }
+  }
 }
+
+
 
 
